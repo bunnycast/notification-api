8f6e551ac26188316851894fa044e7ee3f12b4f3
bunnycast <berzzubunny@gmail.com>
2022-01-31 23:57:27 +0900

    12-1. Test Code_1

---
+++ tests/test_auth.py
@@ -0,0 +1,33 @@
+from app.database.conn import db
+from app.database.schema import Users
+
+
+def test_registration(client, session):
+    """
+    레버로그인
+    :param client:
+    :param session:
+    :return:
+    """
+    user = dict(email="berzzubunny@gmail.com", pw="1111", name="Daniel Kim", phone="01050128587")
+    res = client.post("api/auth/registration/email", json=user)
+    res_body = res.json()
+    print(res.json())
+    assert res.status_code == 201
+    assert "Authorization" in res_body
+
+
+def test_registration_exist_email(client, session):
+    """
+    레버로그인
+    :param client:
+    :param session:
+    :return:
+    """
+    user = dict(email="bunnycast@naver.com", pw="1111", name="이를 본 네티즌", phone="01050128587")
+    db_user = Users.create(session=session, **user)
+    session.commit()
+    res = client.post("api/auth/register/email", json=user)
+    res_body = res.json()
+    assert res.status_code == 400
+    assert "EMAIL_EXISTS" == res_body["msg"]
